3be22744de3ac5a14e47a886bb150b5319f486ef
sameert89 <91889637+sameert89@users.noreply.github.com>
2022-12-20 15:38:27 +0530

    Intial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    var org = 'Alhansat';
+</script>
+<h1>Welcome to the skeleton project for winter internship at {org}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
