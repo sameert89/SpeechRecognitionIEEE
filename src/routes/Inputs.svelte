67b760ec33010d3b78005651da546ce3eead5ae9
Kuvarapuhanoku <hanokukuvarapu4@gmail.com>
2022-12-21 21:36:34 +0530

    First commit

--- src/routes/Inputs.svelte
+++ src/routes/Inputs.svelte
@@ -8,29 +8,64 @@
 	var org = 'Alhansat';
 </script>
 
-<div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Language
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="/">Action</a></li>
-      <li><a class="dropdown-item" href="/">Another action</a></li>
-      <li><a class="dropdown-item" href="/">Something else here</a></li>
-    </ul>
+<div class="droplan">
+  <select id="Language">
+    <option value="1">English</option>
+    <option value="2">हिन्दी</option>
+    <option value="3">తెలుగు</option>
+    <option value="4">मराठी</option>
+    <option value="5">ಕನ್ನಡ</option>
+    <option value="6">தமிழ்</option>
+    <option value="7">മലയാളം</option>
+  </select>
   </div>
-  <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Country
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="/">Action</a></li>
-      <li><a class="dropdown-item" href="/">Another action</a></li>
-      <li><a class="dropdown-item" href="/">Something else here</a></li>
-    </ul>
+  <div class="dropcont">
+    <select id="Country">
+      <option value="IN">India</option>
+      <option value="AU">Australia</option>
+      <option value="US">United States</option>
+      <option value="UK">United kingdom</option>
+      <option value="SA">South Africa</option>
+      <option value="CA">Canada</option>
+      <option value="KE">Kenya</option>
+      <option value="TZ">Tanzania</option>
+      <option value="GH">Ghana</option>
+      <option value="NZ">New Zealand</option>
+      <option value="NG">Nigeria</option>
+      <option value="PH">Philippines</option>
+    </select>
   </div>
-  <button type="button" class="btn btn-primary"><i class="fa-solid fa-download"></i>download text</button>
+  <button type="button" class="btn btn-primary"><i class="fa-solid fa-download"></i>Copy Text</button>
 
 <style>
-    
+
+  select{
+    width: 300px;
+    height: 30px;
+    position: relative;
+    left: 300px;
+    top: 20px;
+  }
+
+  .dropcont{
+    position: relative;
+    left: 330px;
+    top: -30.2px;
+  }
+
+  button{
+    position: relative;
+    left: 1071px;
+    top: -45px;
+    width: 150px;
+    color: #fff;
+    font-weight: bold;
+  }
+
+  button:active{
+    transform: scale(0.95);
+    box-shadow: 2px 2px 5px black;
+  }
+  
 </style>
 
